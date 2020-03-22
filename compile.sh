fb54179cdc4a0e21e58a0b995118cf27cb1b4c7f
Patrick Bowen <phunanon@users.noreply.github.com>
2020-03-22 16:52:53 +0000

    Toolchain: improve multiple programs compilation

--- compile.sh
+++ compile.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 mkdir -p bin
-
-cd corpus/programs
-node ../../compilers/JavaScript/compiler.js init.chi
-mv init.kua ../../bin
-cd ../..
-
+cd bin
+#Copy the init program if it doesn't yet exist
+if [ ! -f init.chi ]; then
+  cp ../corpus/programs/init.chi .
+fi
+#Compile all .chi in the bin folder
+for filename in *.chi; do
+  [ -e "$filename" ] || continue
+  node ../compilers/JavaScript/compiler.js $filename
+done
+cd ..
+#Compile the Chika executable
+#  by temporarily copying Chika_PC.cpp into the Chika_Arduino directory
+#  This is done as Arduino IDE wouldn't ignore the file during compilation
 cp Chika_PC.cpp Chika_Arduino
 cd Chika_Arduino
 g++ -o ../bin/chika Chika_PC.cpp ChVM.cpp Item.cpp utils.cpp config.cpp -g
--- compilers/JavaScript/compiler.js
+++ compilers/JavaScript/compiler.js
@@ -6,6 +6,7 @@ const ramRequest = process.argv[3] != undefined ? parseInt(process.argv[3]) : 81
 const outputFile = inputFile.substr(0, inputFile.lastIndexOf(".")) + ".kua";
 
 function compileFile (err, buf) {
+  process.stdout.write(`${inputFile}\t`);
   const compiled = compile(buf.toString(), ramRequest);
   const image = Buffer.alloc(compiled.image.length / 2, compiled.image, "hex");
   fs.writeFile(outputFile, image, "binary",function(err) { });
